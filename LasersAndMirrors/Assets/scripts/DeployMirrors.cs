ef42dca1e172d005db55804b2e77609c3976aa6e
Yannick <y.wesseloh@freenet.de>
2016-04-29 11:52:38 +0200

    implemented mirror spawning and rotating

---
+++ LasersAndMirrors/Assets/scripts/DeployMirrors.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeployMirrors : MonoBehaviour {
+
+    public GameObject mirror;
+    private float z = 2.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0) && !Camera.main.GetComponent<RotateMirrors>().Rotation)
+        {
+            Vector3 spawnPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, z);
+            Vector3 pos = Camera.main.ScreenToWorldPoint(spawnPos);
+            Instantiate(mirror, pos, Quaternion.identity);
+
+            //Camera.main.getComponent<RotateMirrors>().Rotation = false;
+        }
+	}
+}
