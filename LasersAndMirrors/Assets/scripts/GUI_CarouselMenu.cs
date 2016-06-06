cf408e34b6def75babd9c311b0cd69c5431503d4
Katrin <kati1304@freenet.de>
2016-06-06 09:50:23 +0200

    carousel menu added (as a gamescene)

---
+++ LasersAndMirrors/Assets/scripts/GUI_CarouselMenu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI_CarouselMenu : MonoBehaviour {
+
+    private GameObject carousel;
+    private int degCounter = 0;
+    private float deg = 2.0f; //Drehgeschwindigkeit
+    private bool rotate = false;
+    // Use this for initialization
+    void Start()
+    {
+        carousel = GameObject.Find("Carousel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (Input.mousePosition.x <= Screen.width / 2) //zwischen Links und rechts auf dem Bildschirm unterscheiden
+            {
+                if (!rotate)
+                {
+                    rotate = true;
+                    deg = 2.0f;
+                }
+            }
+            else
+            {
+                if (!rotate)
+                {
+                    rotate = true;
+                    deg = -2.0f;
+                }
+            }
+        }
+
+        if (rotate)
+        {
+            carousel.transform.Rotate(new Vector3(0, deg, 0)); //Animation der Drehung
+            degCounter += (int)((deg < 0) ? deg * (-1) : deg); //Gradzahl wird zum Counter hinzugerechnet, damit nie eine 90 Gradgrenze überschritten wird
+            if (degCounter >= 90)
+            {
+                rotate = false;
+                degCounter = 0; //zurücksetzen des Counters
+            }
+        }
+
+    }
+}
---
+++ LasersAndMirrors/Assets/scripts/Menu_Script.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu_Script : MonoBehaviour {
+
+
+    public int loadedScene = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject clickedGmObj = null;
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickedGmObj = GetClickedGameObject();
+            if (clickedGmObj != null && clickedGmObj.CompareTag("Button"))
+            {
+                Debug.Log("Spiel wird gestartet!");
+                //Jeweilige Szene laden
+                //entweder über den Applicationamager oder über SceneManager (?)
+            }
+        }
+    }
+
+
+    GameObject GetClickedGameObject()
+    {
+        // Builds a ray from camera point of view to the mouse position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        // Casts the ray and get the first game object hit
+        if (Physics.Raycast(ray, out hit, 100))
+            return hit.transform.gameObject;
+        else
+            return null;
+    }
+}
