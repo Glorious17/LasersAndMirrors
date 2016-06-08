97e9fd74890d9de4f315a1a98c12a632321fb7fd
Katrin <kati1304@freenet.de>
2016-06-08 19:47:44 +0200

    improved carouselmenu
    idle floating in midair

---
+++ LasersAndMirrors/Assets/scripts/IdleFloat.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IdleFloat : MonoBehaviour {
+
+    float y = 0;
+    public bool sine = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(sine)
+        transform.position = new Vector3(transform.position.x, 0.3f*Mathf.Sin(y) , transform.position.z);
+        else
+            transform.position = new Vector3(transform.position.x, 0.35f * Mathf.Cos(y), transform.position.z);
+
+        y += 0.04f;
+	}
+}
