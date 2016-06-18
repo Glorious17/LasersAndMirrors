595db7d51432a430826aa4b7ae29d2527dbba854
Katrin <kati1304@freenet.de>
2016-06-18 11:03:42 +0200

    all 3 games integrated
    bug list:
    -der geknetete Draht wird immer noch nicht angezeigt
    -etwas mit der integration mit den punkten und checkpoints klappte nicht
    (lazermaze)

---
+++ LasersAndMirrors/Assets/scripts/scripts_goodcupbadcup/End_GUI.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * This script will show the total score of the player and the way he/she reached
+ * and offers the function to get back to game-menu
+ *
+ */
+
+public class End_GUI : MonoBehaviour {
+	private int score;
+
+	private GUIStyle fontStyleKlein = new GUIStyle();
+	private GUIStyle fontStyleGross = new GUIStyle();
+	public GUIStyle neustart = new GUIStyle();
+	public GUIStyle zurueck = new GUIStyle();
+
+	private float screenWidth = Screen.width;
+	private float screenHeight = Screen.height;
+
+	void start(){
+	}
+
+	void Update(){
+
+	}
+
+
+	void OnGUI() {
+		score = GUI_ScriptGB.score * GUI_ScriptGB.wave;
+		fontStyleKlein.fontSize = (int)screenWidth / 35;
+		fontStyleGross.fontSize = (int)screenWidth / 25;
+		fontStyleKlein.alignment = TextAnchor.MiddleCenter;
+		fontStyleGross.alignment = TextAnchor.MiddleCenter;
+
+		neustart.fixedWidth = Screen.width/6;
+		neustart.fixedHeight = Screen.height/4;
+		zurueck.fixedWidth = Screen.width/8; //80
+		zurueck.fixedHeight = Screen.height / 12; //39
+
+
+
+		//Score usw.
+		GUILayout.BeginArea (new Rect (Screen.width/2-250, Screen.height/5+5, 500, 500));
+		//Die erreichten Werte werden untereinander angezeigt
+		GUILayout.Label ("Wave", fontStyleKlein);
+		GUILayout.Label ("" + GUI_ScriptGB.wave, fontStyleGross);
+		GUILayout.Label ("Score", fontStyleKlein);
+		GUILayout.Label ("" + GUI_ScriptGB.score, fontStyleGross);
+		GUILayout.Label("Final Score", fontStyleKlein);
+		GUILayout.Label ("" + score, fontStyleGross);
+
+		/*if (GUILayout.Button("Menü", pauseButton))
+        {
+            //Wieder zum Menü
+            Application.LoadLevel(0);
+
+        }*/
+		GUILayout.EndArea ();
+		GUILayout.BeginArea (new Rect(Screen.width/2-neustart.fixedWidth/2, Screen.height - Screen.height/4, 500, 500));
+		if (GUILayout.Button ("", neustart)) {
+			Application.LoadLevel(1);
+			GUI_ScriptGB.timeAnz =0;
+			GUI_ScriptGB.score = 0;
+			score = 0;
+			GUI_ScriptGB.wave = 0;
+		}
+		GUILayout.EndArea ();
+
+		GUILayout.BeginArea (new Rect(10, Screen.height-40, 200,200));
+		if(GUILayout.Button("", zurueck)){
+			Application.LoadLevel (0);
+		}
+		GUILayout.EndArea ();
+	}
+}
+
---
+++ LasersAndMirrors/Assets/scripts/scripts_goodcupbadcup/scoreObserverBad.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Process Objects that pass the BadCollider
+ */
+
+public class scoreObserverBad : MonoBehaviour {
+
+	public Camera mainCam;
+    public AudioClip life_down, life_up, rotate, freeze;
+    public AudioSource sounds;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag.Equals ("Bad")) {
+			GUI_ScriptGB.score++;				//Increase score of player if badCup was terminated
+		} else if (other.tag.Equals ("PowerUp")) {
+			switch (other.gameObject.name) {
+			case "OneUp(Clone)":
+				if (GameObject.Find("Main Camera").GetComponent<GUI_ScriptGB>().leben < 5 && GameObject.Find("OneUp(Clone)").GetComponent<CollisionCounter>().collided==false)
+                    {         //if powerUp was OneUp the live will increase
+						GameObject.Find("OneUp(Clone)").GetComponent<CollisionCounter>().collided=true;
+						GameObject.Find ("Main Camera").GetComponent<GUI_ScriptGB> ().leben++;
+                        sounds = GetComponent<AudioSource>();
+                        sounds.clip = life_up;
+                        sounds.Play();
+                    }
+                    
+				break;
+			case "SlowDown(Clone)":
+				if (GameObject.Find ("SlowDown(Clone)").GetComponent<CollisionCounter> ().collided == false) {
+					GameObject.Find("SlowDown(Clone)").GetComponent<CollisionCounter>().collided=true;
+					sounds = GetComponent<AudioSource> ();
+					sounds.clip = freeze;
+					sounds.Play ();
+					GameObject.Find ("Treadmill").GetComponent<TreadmillRegulator> ().slowDownStart = GUI_ScriptGB.timeAnz;	//if powerUp was SlowDown the cups will move slower
+					GameObject.Find ("Treadmill").GetComponent<TreadmillRegulator> ().slowDownActive = true;
+				}
+				break;
+			case "Invert(Clone)":
+				if (GameObject.Find ("Invert(Clone)").GetComponent<CollisionCounter> ().collided == false) {
+					GameObject.Find ("Invert(Clone)").GetComponent<CollisionCounter> ().collided = true;
+					sounds = GetComponent<AudioSource> ();
+					sounds.clip = rotate;
+					sounds.Play ();
+					mainCam.GetComponent<InvertScreen> ().i = 0;
+					GameObject.Find ("Treadmill").GetComponent<TreadmillRegulator> ().slowDownStart = GUI_ScriptGB.timeAnz;		//if powerup was Invert the Camera will rotate with animation
+					GameObject.Find ("Treadmill").GetComponent<TreadmillRegulator> ().slowDownDuration = 1.4f;
+					GameObject.Find ("Treadmill").GetComponent<TreadmillRegulator> ().slowDownActive = true;
+				}
+				break;
+			default:
+				break;
+			}
+			
+		} else {
+            sounds = GetComponent<AudioSource>();
+            sounds.clip = life_down;
+            sounds.Play();
+            
+			GameObject.Find ("Main Camera").GetComponent<GUI_ScriptGB> ().leben--;
+			GameObject.Find ("Main Camera").GetComponent<GUI_ScriptGB> ().lifeCounter = GUI_ScriptGB.timeAnz + 2;
+		}
+		}
+}
