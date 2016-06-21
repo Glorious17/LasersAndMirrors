335c6ba96ed43df69977aec9c41c17fdf537ec29
chili <schokocorny92@googlemail.com>
2016-06-21 20:06:19 +0200

    Credits
    Credits gibt es nun. Sind noch nicht im Menu aufrufbar.
    Noch ein Problem: werden nicht komplett angezeigt bricht irgendwann
    einfach ab dabei..

---
+++ LasersAndMirrors/Assets/scripts/Credit_Script.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class Credit_Script : MonoBehaviour {
+
+	public string path;
+	private TextReader reader;
+	private List<string> credits = new List<string> ();
+	public GUIStyle font = new GUIStyle(); 
+	public GUIStyle back = new GUIStyle();
+	private float scroll = 0;
+
+	// Use this for initialization
+	void Start () {
+		path = "Assets/Texts/Credits.txt";
+		reader = new StreamReader (path);
+		string temp;
+		while((temp = reader.ReadLine()) != null){
+			credits.Add(temp);
+		} 
+		reader.Close ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI(){
+
+		font.alignment = TextAnchor.MiddleCenter;
+		font.fontSize = Screen.width / 39;
+		back.fixedWidth = Screen.width/8; //80
+		back.fixedHeight = Screen.height / 12; //39
+
+		GUILayout.BeginArea (new Rect (Screen.width/2-250, Screen.height-scroll, 500, 500));
+		for (int i = 0; i < 43; i++) {
+			string line = credits [i];
+			GUILayout.Label (line, font);
+		}
+		GUILayout.EndArea ();
+
+		scroll += 0.3f;
+
+		GUILayout.BeginArea (new Rect(10, Screen.height-40, 200,200));
+		if(GUILayout.Button("", back)){
+			SceneManager.LoadScene(0);
+		}
+		GUILayout.EndArea ();
+	}
+}
