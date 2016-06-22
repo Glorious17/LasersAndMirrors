f5c340cccd3109d30dfde3ab9c7219b93cc114f9
chili <schokocorny92@googlemail.com>
2016-06-22 17:07:27 +0200

    Kleine Änderng an den credits

--- LasersAndMirrors/Assets/scripts/Credit_Script.cs
+++ LasersAndMirrors/Assets/scripts/Credit_Script.cs
@@ -9,38 +9,40 @@ public class Credit_Script : MonoBehaviour {
 	public string path;
 	private TextReader reader;
 	private List<string> credits = new List<string> ();
-	public GUIStyle font = new GUIStyle(); 
+	private GUIStyle font = new GUIStyle(); 
 	public GUIStyle back = new GUIStyle();
 	private float scroll = 0;
+	private string line;
 
 	// Use this for initialization
 	void Start () {
-		path = "Assets/Resources/Credits.txt";
+		path = Application.dataPath+"/Resources/Credits.txt";
 		reader = new StreamReader (path);
 		string temp;
 		while((temp = reader.ReadLine()) != null){
 			credits.Add(temp);
 		} 
 		reader.Close ();
+		for (int i = 0; i < 43; i++) {
+			line += credits [i] + "\n";
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
 	void OnGUI(){
 
 		font.alignment = TextAnchor.MiddleCenter;
-		font.fontSize = Screen.width / 39;
+		font.fontSize = Screen.width / 37;
+		font.normal.textColor = Color.white;
 		back.fixedWidth = Screen.width/8; //80
 		back.fixedHeight = Screen.height / 12; //39
 
 		GUILayout.BeginArea (new Rect (Screen.width/2-250, Screen.height-scroll, 500, 1500));
-		for (int i = 0; i < 43; i++) {
-			string line = credits [i];
-			GUILayout.Label (line, font);
-		}
+		GUILayout.Label (line, font);
 		GUILayout.EndArea ();
 
 		scroll += 0.3f;
